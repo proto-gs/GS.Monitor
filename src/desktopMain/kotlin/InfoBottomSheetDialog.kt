e573d61140a60a459eeea5007b00d5e500c7a2fa
proto-gs <georgsmerdov@outlook.com>
2026-08-14 22:12:23 +1000

    update infobottomsheetdialog: add icon and title app

--- src/desktopMain/kotlin/InfoBottomSheetDialog.kt
+++ src/desktopMain/kotlin/InfoBottomSheetDialog.kt
@@ -1,12 +1,16 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
@@ -15,9 +19,11 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,12 +42,43 @@ fun InfoBottomSheetDialog(
     AlertDialog(
         onDismissRequest = onDismiss,
         title = {
-            Text(
-                text = "Application information",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                color = dropdownTextColor
-            )
+            // Элементы выстроены по левому краю в нужном порядке
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+
+                Text(
+                    text = "Application information",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = dropdownTextColor
+                )
+
+
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                        .background(Color.White),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource("ic_gs_ht.png"),
+                        contentDescription = "Аватар",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+
+                Text(
+                    text = "GS Monitor",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Black,
+                    color = dropdownTextColor
+                )
+            }
         },
         text = {
             Column(
